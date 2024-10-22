9eeafefd83da8a3019927099d839ff198a1a2c0d
Prateek-1998 <103034020+Prateek-1998@users.noreply.github.com>
2024-10-22 20:51:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import  React from 'react';
+import './App.css';
+import VendorDetails from './VendorDetails'
+
+
+
+
+function App() {
+  // const formik={onsubmit }
+  // const handleSubmit = (values) => {
+  //   console.log('Form Data:', values);
+  // };
+  return (
+    <div>
+      
+      
+      <VendorDetails/>
+     
+      
+    </div>
+  );
+}
+
+export default App;
